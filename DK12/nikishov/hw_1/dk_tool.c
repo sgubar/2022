7eec0d2ec8f1f05a35999288f23e53e4c4e8249f
dk12in <90895989+dk12in@users.noreply.github.com>
2022-02-02 22:19:56 +0200

    Update dk_tool.c

--- DK12/nikishov/hw_1/dk_tool.c
+++ DK12/nikishov/hw_1/dk_tool.c
@@ -20,11 +20,64 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "dk_tool.h"
 
-void matrix_add(const unsigned int i, const unsigned int j, const int * ma, const int * mb, int * res)
+matrix matrix_init(int m, int n)
 {
-	for(unsigned int c = 0; c < i*j; c++)
-		*(res + c) = *(ma + c) + *(mb + c);
+	matrix tmp;
+	tmp.m = m;
+	tmp.n = n;
+	tmp.mat = (element**) malloc(m*sizeof(element*));
+	for(int i = 0; i < m; i++)
+		tmp.mat[i] = malloc(n*sizeof(element));
+	return tmp;
 }
 
+void matrix_destroy(matrix * mat)
+{
+	for(int i = 0; i < mat->m; i++)
+		free(mat->mat[i]);
+	free(mat->mat);
+}
+
+
+void matrix_fill(matrix * mat, element x)
+{
+	for(int i = 0; i < mat->m; i++)
+		for(int j = 0; j < mat->n; j++)
+			mat->mat[i][j] = x;
+}
+
+void matrix_enter(matrix * mat, char s)
+{
+	printf("Please enter matrix %c:\n", s);
+	for(int i = 0; i < mat->m; i++) {
+		for(int j = 0; j < mat->n; j++) {
+			printf("%c[%d][%d]: ", s, i, j);
+			scanf("%lf", &mat->mat[i][j]);
+		}
+		putchar('\n');
+	}
+}
+
+_Bool matrix_add(const matrix * a, const matrix * b, matrix * res)
+{
+	if(a->m != b->m || a->n != b->n) return 1;
+	if(a->m != res->m || a->n != res->n) return 1;
+	for(int i = 0; i < a->m; i++)
+		for(int j = 0; j < a->n; j++)
+			res->mat[i][j] = a->mat[i][j] + b->mat[i][j];
+	return 0;
+}
+
+void matrix_print(const matrix * mat)
+{
+	for(int i = 0; i < mat->m; i++) {
+		for(int j = 0; j < mat->n; j++)
+			printf("%3.2lf ", mat->mat[i][j]);
+		putchar('\n');
+	}
+}

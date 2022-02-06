789531a41d534d945ccc40ccde09839d877c3161
Ruslana29 <91050763+Ruslana29@users.noreply.github.com>
2022-02-06 21:59:25 +0200

    Update dk_tool.h

--- DK11/Bondar Ruslana/dk_tool.h
+++ DK11/Bondar Ruslana/dk_tool.h
@@ -1,1 +1,16 @@
+#ifndef DK_TOOL_H_INCLUDED
+#define DK_TOOL_H_INCLUDED
+
+
+int m, n, p, q, c, d, k, sum ;//задаємо змінні
+  int first[10][10], second[10][10], multiply[10][10];//задаємо матриці
+
+void printf_size1 ();
+void matrix1_elements ();
+void printf_size2 ();
+void matrix2_elements ();
+void multiple ();
+void result ();
+
+#endif // DK_TOOL_H_INCLUDED
 

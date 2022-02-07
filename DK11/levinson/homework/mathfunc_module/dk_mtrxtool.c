6c675fe7b7f78f70a18896f9909a7ca331810a43
George Levinson <gosha.levinson@gmail.com>
2022-02-07 14:21:26 +0200

    Implemented ctor and dtor. Specified the invalid Matrix condition.

--- DK11/levinson/homework/mathfunc_module/dk_mtrxtool.c
+++ DK11/levinson/homework/mathfunc_module/dk_mtrxtool.c
@@ -1,8 +1,36 @@
+#include <stdlib.h>
 #include "dk_mtrxtool.h"
 
-void mtrx_dtor(Matrix);
-Matrix mtrx_ctor(int width, int height, int* values);
-Matrix mtrx_prod(Matrix, Matrix);
+void mtrx_dtor(Matrix m)
+{
+    free(m.ptr);
+};
+Matrix mtrx_ctor(int width, int height, int values[width*height])
+{
+    Matrix result;
+    int **data = malloc(sizeof(int*) * height);
+
+    for(int i = 0; i < height; i++)
+    {
+        int *p = &values[width * (i + 1)];
+        data[i] = p;
+    }
+
+    result.height = height;
+    result.width = width;
+    result.ptr = data;
+
+    return result;
+};
+Matrix mtrx_prod(Matrix left, Matrix right)
+{
+    Matrix result;
+
+    if(left.width != right.height)
+    {
+        result.ptr = NULL;
+    }
+};
 Matrix mtrx_num_prod(Matrix, int);
 Matrix mtrx_sum(Matrix,Matrix);
 Matrix mtrx_diff(Matrix,Matrix);
--- DK11/levinson/homework/mathfunc_module/dk_mtrxtool.h
+++ DK11/levinson/homework/mathfunc_module/dk_mtrxtool.h
@@ -3,13 +3,14 @@
 
 typedef struct Matrix
 {
+    // (ptr == NULL) indicates that a function returnig Matrix type did not execute properly (e.g. when addition of differently sized matrices is attempted)
     int **ptr;
     int height;
     int width;
 } Matrix;
 
 void mtrx_dtor(Matrix);
-Matrix mtrx_ctor(int width, int height, int* values);
+Matrix mtrx_ctor(int width, int height, int values[width*height]);
 Matrix mtrx_prod(Matrix, Matrix);
 Matrix mtrx_num_prod(Matrix, int);
 Matrix mtrx_sum(Matrix,Matrix);

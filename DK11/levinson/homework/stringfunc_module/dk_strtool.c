899de60106dd9c813c307de0251c477c9ca2c57e
George Levinson <gosha.levinson@gmail.com>
2022-02-10 19:14:06 +0200

    Created header file for the str module. Populated function signatures.

---
+++ DK11/levinson/homework/stringfunc_module/dk_strtool.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "dk_strtool.h"
+
+char* str_trim(char*);
+char* str_search(char*);
+char* str_reverse(char*);
+int str_count_char(char*);
+int str_count_wrd(char*);
+int str_count_substr(char*);
---
+++ DK11/levinson/homework/stringfunc_module/dk_strtool.h
@@ -0,0 +1,11 @@
+#ifndef DK_STRTOOL
+#define DK_STRTOOL
+
+char* str_trim(char*);
+char* str_search(char*);
+char* str_reverse(char*);
+int str_count_char(char*);
+int str_count_wrd(char*);
+int str_count_substr(char*);
+
+#endif

635da294d3d120f066bdf76b0a073e9532d665b0
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-02-11 10:12:51 +0200

    Create homework.c

---
+++ DK12/Polovyan/hw_1/homework.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "matrix.h"
+
+int main()
+{
+    int n = 0;
+	printf("Введіть розмір матриць для множення: ");
+	scanf("%d", &n);
+	int arr1[n*n], arr2[n*n], arr3[n*n];
+	
+	for(int i = 0; i < n*n ; i++)
+	{
+		printf("\n Введіть %d число першої матриці: ", i+1);
+		scanf("%d", &arr1[i]);
+	}
+	
+		for(int i = 0; i < n*n ; i++)
+		{
+			printf("\n Введіть %d число другої матриці: ", i+1);
+			scanf("%d", &arr2[i]);
+		}
+		
+			matrix1(n, arr1);
+			matrix2(n, arr2);
+			matrix3(n, arr1, arr2, arr3);
+		    
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About

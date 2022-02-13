91c38a50d63dacc4565dacdafcb8f9c8edcde6f7
Ruslana29 <91050763+Ruslana29@users.noreply.github.com>
2022-02-13 18:04:42 +0200

    Create main.c

---
+++ DK11/Bondar Ruslana/Howework/work1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ #include "dk_tool.h"
+int main()
+{
+  printf_size1();
+  matrix1_elements();
+  printf_size2 ();
+  matrix2_elements();
+multiple ();
+result ();
+return 0;
+}

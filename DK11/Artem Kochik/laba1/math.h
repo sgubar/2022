714d27a3702e9bc05330134beea20b3b58445b5e
AKSdjjcSKA <98762662+AKSdjjcSKA@users.noreply.github.com>
2022-02-14 20:34:36 +0200

    Create math.h

---
+++ DK11/Artem Kochik/laba1/math.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "math.h"
+int main ()
+{
+  printf("Результат колькулятора:%ld\n",q(4,4,4));
+printf("3^3:%f\n", power(3,3));
+printf ("5!:%l\n", factorial(5));
+return 0;
+}
+  

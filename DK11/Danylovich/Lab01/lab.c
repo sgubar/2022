797c22b451aa06896cb72d11b2545b6021d7aaf4
Lay <91009566+LayTreE@users.noreply.github.com>
2022-02-14 11:05:52 +0200

    Update lab.c

--- DK11/Danylovich/Lab01/lab.c
+++ DK11/Danylovich/Lab01/lab.c
@@ -4,23 +4,8 @@
 
 int main(int argc, const char * argv[])
 {
-    float arr[4];float j;
-    if(argv[1]==NULL || argv[2]==NULL || argv[3]==NULL)
-    {
-        return 0;
-    }else
-    for(int i=0;i<4;i++)
-    {
-        arr[i]=atoi(argv[i]);
-    }
-    float a=arr[1];float b=arr[2];float c=arr[3];
-    float qq = square(a) + square(b);
-    float s = b + math_pow(c);
-    if(s==0){
-        return 0;
-    }else
-    j = qq/s;
-    float kk = factor(b);
-    float p = j+kk;
-    printf("Answer:%f\n",p);
+    float a = param(1);
+    float b = param(2);
+    float c = param(3);
+    printf("Answer:%d\n",answer(a,b,c));
 }

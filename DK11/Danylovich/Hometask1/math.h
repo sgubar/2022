e00eae751a18ca89aea1cae66e3e5def8b1d7512
Lay <91009566+LayTreE@users.noreply.github.com>
2022-02-14 10:15:05 +0200

    Update math.h

--- DK11/Danylovich/Hometask1/math.h
+++ DK11/Danylovich/Hometask1/math.h
@@ -3,9 +3,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <cs50.h>
 
+int dimensions();
 int **create_matrix(int row, int col);
-void printarray(int **matrix,int n);
+void printarray(int **matrix,int a,int b);
 void delete_matrix(int **matrix, int row);
+void transmat(int **matrix,int a,int b);
+void fillarray(int **matrix,int a,int b);
 
 #endif 

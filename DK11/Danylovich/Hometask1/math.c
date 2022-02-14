f299d45e05e3ab3c7e076b95e973b6aad995f437
Lay <91009566+LayTreE@users.noreply.github.com>
2022-02-14 10:14:41 +0200

    Update math.c

--- DK11/Danylovich/Hometask1/math.c
+++ DK11/Danylovich/Hometask1/math.c
@@ -1,5 +1,13 @@
 #include "math.h"
 
+int dimensions()
+{
+    int a;
+    printf("Введіть розмір матриці: \n");
+    scanf("%d",&a);
+    return a;
+}
+
 int **create_matrix(int row, int col)
 {
 	int **matrix = (int **)malloc(sizeof(int *) * row); // int *array[];
@@ -23,18 +31,52 @@ void delete_matrix(int **matrix, int row) {
 	free(matrix);
 }
 
-void printarray(int **matrix,int n)
+void printarray(int **matrix,int a,int b)
 {
     int i=0,j=0;
-    while(i<n)
+    while(i<a)
     {
         j=0;
-        while(j<3)
+        while(j<b)
         {
             printf("%d\t", *(*(matrix + i) + j));
             j++;
         }
     i++;
     printf("\n");
+    }
+}
+
+void transmat(int **matrix,int a,int b)
+{
+    int n=0, m=0;
+
+    while(n<a-1)
+    {
+        m=0;
+        while(m<b)
+        {
+            if(n==1 && m==0)
+            {
+                m++;
+            }else{
+            int k = *(*(matrix + n) + m);
+            *(*(matrix + n) + m) = *(*(matrix + m) + n);
+            *(*(matrix + m) + n) = k;
+            m++;
+            }
+        }
+    n++;
+    }
+}
+
+void fillarray(int **matrix,int a,int b)
+{
+    for(int i=0;i<a;i++)
+    {
+        for(int j=0;j<b;j++)
+        {
+            *(*(matrix + i) + j) = get_int("a[%d][%d]=",i,j);
+        }
     }
 }

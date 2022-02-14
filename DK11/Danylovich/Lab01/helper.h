4a059b5c23c46aabc5aad27bc9aec1b75a376cbc
Lay <91009566+LayTreE@users.noreply.github.com>
2022-02-14 11:05:28 +0200

    Update helper.h

--- DK11/Danylovich/Lab01/helper.h
+++ DK11/Danylovich/Lab01/helper.h
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 
-int square(int a);
-int math_pow(int c);
-int factor(int a);
+int square(float a);
+int math_pow5(float c);
+int factor(float a);
+int param(int a);
+int answer(float a,float b,float c);
 
 #endif 

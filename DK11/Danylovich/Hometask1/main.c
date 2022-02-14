0f67b1441889d9e9a365a296411e4c407a6c8dcc
Lay <91009566+LayTreE@users.noreply.github.com>
2022-02-14 10:14:09 +0200

    Update main.c

--- DK11/Danylovich/Hometask1/main.c
+++ DK11/Danylovich/Hometask1/main.c
@@ -4,38 +4,17 @@
 #include <cs50.h>
 #include "math.h"
 
-int main()
+int main(int argc, const char * argv[])
 {
-    int **matrix = create_matrix(3,3);
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-            *(*(matrix + i) + j) = get_int("a[%d][%d]=",i,j);
-        }
-    }
-printf("Матриця\n");
-printarray((int **)matrix,3);
-int n=0;int m=0;
-    while(n<2)
-    {
-        m=0;
-        while(m<3)
-        {
-            if(n==1 && m==0)
-            {
-                m++;
-            }else{
-            int k = *(*(matrix + n) + m);
-            *(*(matrix + n) + m) = *(*(matrix + m) + n);
-            *(*(matrix + m) + n) = k;
-            m++;
-            }
-        }
-    n++;
-    }
+    int a= dimensions();
+    int b= dimensions();
+    int **matrix = create_matrix(a,b);
+    fillarray((int **)matrix,a,b);
+    printf("Матриця\n");
+    printarray((int **)matrix,a,b);
+    transmat((int **)matrix,a,b);
     printf("Транспонована Матриця\n");
-    printarray((int **)matrix,3);
-    delete_matrix((int **)matrix,3);
+    printarray((int **)matrix,a,b);
+    delete_matrix((int **)matrix,a);
     return 0;
 }

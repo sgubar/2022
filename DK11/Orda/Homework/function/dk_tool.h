bc983d19dfd022c5c99c0c2ba4d534cd9e7f59fc
Caro <86561352+ordaoleksii@users.noreply.github.com>
2022-02-15 15:42:36 +0200

    Add files via upload

---
+++ DK11/Orda/Homework/function/dk_tool.h
@@ -0,0 +1,12 @@
+#ifndef DK_TOOL_H_INCLUDED
+#define DK_TOOL_H_INCLUDED
+
+int row, col, num;
+int matrix[10][10];
+
+void input_size();
+void input_value();
+void count();
+void output_matrix();
+
+#endif

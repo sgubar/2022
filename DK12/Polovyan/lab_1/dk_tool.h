43b86cea3a8d90c4bcceb6d4fb71f1ab4596eb26
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-02-16 23:02:33 +0200

    Update dk_tool.h

--- DK12/Polovyan/lab_1/dk_tool.h
+++ DK12/Polovyan/lab_1/dk_tool.h
@@ -1,9 +1,18 @@
 #ifndef _TOOLS_H
 #define _TOOLS_H
 
-    float GetCheck(void);
-    float fact(float);
-    float up(float);
-    float res(void);
- 
+   int get_A(void);
+   float get_B(void);
+   float get_C(void);
+   
+   float check_A(int A);
+   float check_B(float B);
+   
+   int factorial(int A);
+   float up(int A, float B, float C);
+   float calc(float upp, int fact, int A, float C);
+   
+   void show_value(float result);
+   
+   
 #endif

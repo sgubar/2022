84358c968525639bf335cd133483f3faa39eb57e
Sochnyi <91053509+Sochnyi@users.noreply.github.com>
2022-02-16 21:59:03 +0200

    Update main.c

--- DK11/Сергій Сопчинський/Практичні роботи.md/Лабораторна №1/main.c
+++ DK11/Сергій Сопчинський/Практичні роботи.md/Лабораторна №1/main.c
@@ -1,1 +1,9 @@
-
+#include <stdio.h>
+#include "math.h"
+int main(int argc, const char * argv[]) 
+	{
+	printf("Hello, World!\n");
+	
+	printf("The result of calculation: %f\n", q(6, 21, 2));
+	return 0;
+	}

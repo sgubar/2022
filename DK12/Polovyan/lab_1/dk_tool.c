33da509ae0c752e0a51e95945ca863376665d5ea
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-02-16 23:03:23 +0200

    Update dk_tool.c

--- DK12/Polovyan/lab_1/dk_tool.c
+++ DK12/Polovyan/lab_1/dk_tool.c
@@ -2,29 +2,49 @@
 #include <math.h>
 #include "tools.h"
 
-    float A, B, C, up1, fact1;
-
-
-
- float GetCheck(void)
- {
-    printf("Введіть A > 0:\n");
-    scanf("%f", &A);
-    printf("Введіть B:\n");
-    scanf("%f", &B);
-    printf("Введіть C =! 0:\n");
-    scanf("%f", &C);
-     if((A<1 && A%1 != 0)|| C <= 0)
+    int get_A(void)
     {
-       printf("ПОМИЛКА !!!\n"); 
-       return 0;
+        int A;
+        printf("Введіть A:\n");
+        scanf("%d", &A);
+        check_A(A);
+        return A;
     }
- }
- 
-//////////////////////////////////////////
-
-    float fact(float A)
+    
+    float get_B(void)
     {
+        float B;
+        printf("Введіть B:\n");
+        scanf("%f", &B);
+        return B;
+    }
+   
+    int get_C(void)
+    {
+        float C;
+        printf("Введіть C:\n");
+        scanf("%f", &C);
+        check_C(C);
+        return C;
+    }
+
+    int check_A(int A)
+    {
+        if(A<1 && A%1 != 0) return 0;
+        printf("А ПОВИННО БУТИ ЦІЛИМ ЧИСЛОМ І БІЛЬШЕ НУЛЯ !!!\n");
+        exit(0);
+    }
+    
+    float check_C(float C)
+    {
+        if(C = 0) return 0;
+        printf("C ПОВИННО НЕ ДОРІВНЮВАТИ 0 !!!\n");
+        exit(0);
+    }
+
+    int factorial(int A)
+    {
+        int fact1;
         for(int d = 0; d <= A; d++) 
         {
             fact1 = fact1 + pow(2, d);
@@ -32,26 +52,21 @@
         return fact1;
     }
     
-////////////////////////////////////////
-
-    float up(float A, float B, float C)
+    float up(int A, float B, float C)
     {
-        up1 = A*C - B;
+        float up1 = A*C - B;
         if(up1 < 0) up1 = up1*(-1);
         return up1;
     }
-
-///////////////////////////////////////////
-
-
-    float res()
+    
+    float calc(float upp, int fact, int A, float C)
     {
-        float res1;
-        GetCheck();
-        fact();
-        up();
-        res1 = (up1/(C*C*C))*fact1;
-        return res1;
+        float calc1 = (upp/(C*C*C)*fact1);
+        return calc1;
+    }
+    
+    void show_value(float result)
+    {
+        printf("Результат: %f \n", result);
     }
 
-///////////////////////////////////////////

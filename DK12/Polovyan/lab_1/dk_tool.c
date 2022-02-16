d172e5fa779bd4ecc05f408cde6871b1acfff75c
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-02-16 18:38:21 +0200

    Update dk_tool.c

--- DK12/Polovyan/lab_1/dk_tool.c
+++ DK12/Polovyan/lab_1/dk_tool.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <math.h>
+#include "tools.h"
 
+    float A, B, C, up1, fact1;
+
+
+
+ float GetCheck(void)
+ {
+    printf("Введіть A > 0:\n");
+    scanf("%f", &A);
+    printf("Введіть B:\n");
+    scanf("%f", &B);
+    printf("Введіть C =! 0:\n");
+    scanf("%f", &C);
+     if((A<1 && A%1 != 0)|| C <= 0)
+    {
+       printf("ПОМИЛКА !!!\n"); 
+       return 0;
+    }
+ }
+ 
+//////////////////////////////////////////
+
+    float fact(float A)
+    {
+        for(int d = 0; d <= A; d++) 
+        {
+            fact1 = fact1 + pow(2, d);
+        }
+        return fact1;
+    }
+    
+////////////////////////////////////////
+
+    float up(float A, float B, float C)
+    {
+        up1 = A*C - B;
+        if(up1 < 0) up1 = up1*(-1);
+        return up1;
+    }
+
+///////////////////////////////////////////
+
+
+    float res()
+    {
+        float res1;
+        GetCheck();
+        fact();
+        up();
+        res1 = (up1/(C*C*C))*fact1;
+        return res1;
+    }
+
+///////////////////////////////////////////

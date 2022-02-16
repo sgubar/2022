4799c6e19e28c75235ffcbc8c3400d00367294c8
Sochnyi <91053509+Sochnyi@users.noreply.github.com>
2022-02-16 22:00:06 +0200

    Update math.h

--- DK11/Сергій Сопчинський/Практичні роботи.md/Лабораторна №1/math.h
+++ DK11/Сергій Сопчинський/Практичні роботи.md/Лабораторна №1/math.h
@@ -1,1 +1,10 @@
+#ifndef math_h
+#define math_h
 
+#include <stdio.h>
+
+double abs_extend (double A);
+double power (int base, int pow); 
+double q (int A, int B, int C); 
+double sum_suma(int A, int D, double res);
+#endif

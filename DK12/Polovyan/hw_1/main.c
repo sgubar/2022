4beb420bd4e53aea4792d88da4292b9c5edd08c7
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-02-18 11:47:56 +0200

    Update main.c

--- DK12/Polovyan/hw_1/main.c
+++ DK12/Polovyan/hw_1/main.c
@@ -3,14 +3,15 @@
 
 int main()
 {
-    int n = get_N();
-	int arr1[n*n], arr2[n*n], arr3[n*n];
+    int n = get_N(); //n*n - matrix size
+	int arr3[n*n], arr2[n*n], arr1[n*n];
 	
 	arr1[n*n] = get_matrix1(n, arr1);
 	arr2[n*n] = get_matrix2(n, arr2);
 		
-	matrix1(n, arr1);
-	matrix2(n, arr2);
-	matrix3(n, arr1, arr2, arr3);		    
+	print_matrix1(n, arr1);
+	print_matrix2(n, arr2);
+	print_matrix3(n, arr1, arr2, arr3);
+		    
 }
 

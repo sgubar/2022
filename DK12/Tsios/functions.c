58ae1dd3c6966f8f817000440719377c30aa302c
tsiosn <99125681+tsiosn@users.noreply.github.com>
2022-02-18 11:24:07 +0200

    Create functions.c

---
+++ DK12/Tsios/functions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void matrixsum(int* matrix_1, int* matrix_2, int row_1, int col_1) {
+
+    for (int i = 0; i < row_1; i++) {                                                             //СУММА МАТРИЦ
+        for (int j = 0; j < col_1; j++) {
+            *(matrix_1 + i * col_1 + j) = *(matrix_1 + i * col_1 + j) + *(matrix_2 + i * col_1 + j);
+        }
+    }
+}

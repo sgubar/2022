5019c7030746cb8c8e2d5cb0675fc90c2ca7eac1
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-02-18 11:14:30 +0200

    Update main.c

--- DK12/Polovyan/hw_1/main.c
+++ DK12/Polovyan/hw_1/main.c
@@ -3,26 +3,14 @@
 
 int main()
 {
-    int n = 0;
-	printf("Введіть розмір матриць для множення: ");
-	scanf("%d", &n);
+    int n = get_N();
 	int arr1[n*n], arr2[n*n], arr3[n*n];
 	
-	for(int i = 0; i < n*n ; i++)
-	{
-		printf("\n Введіть %d число першої матриці: ", i+1);
-		scanf("%d", &arr1[i]);
-	}
-	
-		for(int i = 0; i < n*n ; i++)
-		{
-			printf("\n Введіть %d число другої матриці: ", i+1);
-			scanf("%d", &arr2[i]);
-		}
+	arr1[n*n] = get_matrix1(n, arr1);
+	arr2[n*n] = get_matrix2(n, arr2);
 		
-			matrix1(n, arr1);
-			matrix2(n, arr2);
-			matrix3(n, arr1, arr2, arr3);
-		    
+	matrix1(n, arr1);
+	matrix2(n, arr2);
+	matrix3(n, arr1, arr2, arr3);		    
 }
 

e458a10bed2dba0f26c95ffb8807eebad31c42e8
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-02-18 11:27:37 +0200

    Update dk_tool.h

--- DK12/Polovyan/lab_1/dk_tool.h
+++ DK12/Polovyan/lab_1/dk_tool.h
@@ -5,14 +5,21 @@
 #ifndef _DK_TOOL_H
 #define _DK_TOOL_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
 	int get_A();
 	void check_A(int A);
 	float get_B();
 	float get_C();
 	void check_C(float C);
-	void res(float up1, float C, int fact1);
 	float calc_up1(int A, float B, float C);
 	int calc_fact1(int A);
-
+	void result(float up1, float C, int fact1);
+	
+#ifdef __cplusplus
+}
+#endif
 
 #endif

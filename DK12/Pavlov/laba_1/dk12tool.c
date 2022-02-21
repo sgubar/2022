34c282994531b9bc4f1f1a879916842b79217455
1lp1c <90896892+1lp1c@users.noreply.github.com>
2022-02-21 22:27:50 +0300

    Update dk12tool.c

--- DK12/Pavlov/laba_1/dk12tool.c
+++ DK12/Pavlov/laba_1/dk12tool.c
@@ -3,38 +3,49 @@
 #include <stdlib.h>
 #include "dk12tool.h"
 
-float a, b, c;
-float data = 0;
-
-int enter(void)
+int enter(float *a, float *b, float *c)
 {
    printf("Enter a:");
-   scanf("%f", &a);
+   scanf("%f", &*a);
    printf("Enter c:");
-   scanf("%f", &c);
+   scanf("%f", &*c);
    printf("Enter b:");
-   scanf("%f", &b);
-   if (b % 1 != 0)
+   scanf("%f", &*b);
+   int newb = ceil(*b);
+   if (*b - newb != 0)
    {
-       printf("!FATAL ERROR! \nEnter b as integer value");
-       return 0;
+     printf("FATAL ERROR, B MUST BE INTEGER");
+     return 0;
    }
-   
-}
+   return 1;
 
+}
 int factorial(float fac)
 {
   return (fac < 2) ? 1 : fac * factorial (fac - 1);
 }
 
-float calc()
-{   
-    float x;
-    enter();
-    for(int d = 0; d<=b; d++)
+float sum(float b)
+{
+  int data = 0;
+  for(int d = 0; d<=b; d++)
     {
         data += factorial(d);
     }
-    x = (a*b)/(b + pow(c,2)) + data;
-    return x;
+    return data;
+}
+
+
+float calc(float a, float b, float c)
+{
+    float x;
+    if (enter(&a, &b, &c) == 1)
+    {
+      x = (a*b)/(b + pow(c,2)) + sum(b);
+      return x;
+    }
+    else 
+    {
+      return -1;
+    }
 }

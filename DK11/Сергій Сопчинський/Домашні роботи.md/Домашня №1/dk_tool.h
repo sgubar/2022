e97956c2d3b557b899f53439ae83ce3a80f80764
Sochnyi <91053509+Sochnyi@users.noreply.github.com>
2022-04-07 17:45:00 +0300

    Update dk_tool.h

--- DK11/Сергій Сопчинський/Домашні роботи.md/Домашня №1/dk_tool.h
+++ DK11/Сергій Сопчинський/Домашні роботи.md/Домашня №1/dk_tool.h
@@ -1,1 +1,12 @@
-
+#ifndef 
+#define 
+#include <stdio.h>
+int m, n, p, q, c, d, k, sum;
+int first[6][6], second[6][6], multiply[6][6];
+void printf_size1 ();
+void matrix1_elements ();
+void printf_size2 ();
+void matrix2_elements ();
+void multiple ();
+void result ();
+#endif

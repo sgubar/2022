7d920587098d9b2f89f7ea97ff437af58d34e785
Sochnyi <91053509+Sochnyi@users.noreply.github.com>
2022-04-07 17:41:27 +0300

    Update main.c

--- DK11/Сергій Сопчинський/Домашні роботи.md/Домашня №1/main.c
+++ DK11/Сергій Сопчинський/Домашні роботи.md/Домашня №1/main.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+#include "dk_tool.h"
+int main()
+	{
+  	printf_size1(); рядок
+  	matrix1_elements(); цифра р
+ 	printf_size2 (); стовпчик
+  	matrix2_elements(); цифра с
+	multiple ();
+	result ();
+	return 0;
+	}

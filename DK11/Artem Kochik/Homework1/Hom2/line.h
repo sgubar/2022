4ac59a96282f3130be11da31cdd3369173f3723b
Artemstudent123 <90600280+Artemstudent123@users.noreply.github.com>
2022-04-15 10:43:50 +0300

    Create line.h

---
+++ DK11/Artem Kochik/Homework1/Hom2/line.h
@@ -0,0 +1,23 @@
+#ifndef line_h
+#define line_h
+
+#include <stdio.h>
+#include "point.h"
+
+typedef struct tag_line {
+    Point *A;
+    Point *B;
+} Line;
+
+Line *createLineByPoints(Point *p1, Point *p2);
+Line *createLineByCoords (int x1, int x2, int y1, int y2);
+void deleteLine(Line *l);
+
+void printLine (Line *l);
+Line *copyLine(Line *l);
+int isEqualLine(Line *ll, Line *rl);
+
+double lenghtLine(Line *l);
+
+
+#endif /*line_h */

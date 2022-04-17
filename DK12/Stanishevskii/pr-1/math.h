7db312fa4a586e2c0bbefdb7e91b0a62f1ec1c3a
GettoKatleta <90596321+GettoKatleta@users.noreply.github.com>
2022-04-17 22:10:14 +0200

    Create math.h

---
+++ DK12/Stanishevskii/pr-1/math.h
@@ -0,0 +1,10 @@
+#ifndef MATH_H_INCLUDED
+#define MATH_H_INCLUDED
+int d;
+int value(int a);
+long sum_factorial(int start, int finish, int x);
+long factorial(int number);
+double q(int a, int b, int c);
+
+
+#endif 

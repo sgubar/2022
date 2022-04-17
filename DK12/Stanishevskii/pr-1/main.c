c5df715b9dfd2e6ee99e4c942d03f25b204e4945
GettoKatleta <90596321+GettoKatleta@users.noreply.github.com>
2022-04-17 22:09:45 +0200

    Create main.c

---
+++ DK12/Stanishevskii/pr-1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "math.h"
+#include <math.h>
+
+int main()
+{
+    int a = value(1);
+    int b = value(2);
+    int c = value(3);
+    if (b == 0)
+    {
+        printf(" Mat oshibka \n");
+    }
+    else {
+        printf(" Q = %f", q(a, b, c));
+
+        return 0;
+    }
+}

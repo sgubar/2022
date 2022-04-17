e17ae74bfc6596882715a01389f120d672c8449e
Lay <91009566+LayTreE@users.noreply.github.com>
2022-04-17 21:48:44 +0300

    Create point.h

---
+++ DK11/Danylovich/Hometask2/point.h
@@ -0,0 +1,21 @@
+#ifndef point_h
+#define point_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "circle.h"
+#include "point.h"
+
+typedef struct tag_point{
+    int x;
+    int y;
+}Point;
+
+Point *createPoint(int x,int y);
+void deletePoint(Point *p);
+void printPoint(Point *p);
+Point *copyPoint(Point *p);
+int isEqualPoints(Point *lp,Point *rp);
+
+#endif 

85a71f161f7f58f2de927bc99688093ad0e90b56
Ruslana29 <91050763+Ruslana29@users.noreply.github.com>
2022-04-19 21:48:27 +0300

    Create tree.c

---
+++ DK11/Bondar Ruslana/Howework/work5/tree.c
@@ -0,0 +1,220 @@
+#include "tree.h"
+#include <stdlib.h>
+
+static void destroyNode(FloatNode *aNode);
+static FloatNode *createFloatNodeWithValue(float aValue);
+static FloatNode *getSuccessor(FloatTree *tree, FloatNode *toDelete);
+static void print_float_node(FloatNode *node);
+
+FloatTree *createFloatTree()
+{
+	FloatTree *theTree = (FloatTree *)malloc(sizeof(FloatTree));
+	if (NULL != theTree)
+	{
+		theTree->root = NULL;
+		theTree->count = 0;
+	}
+
+	return theTree;
+}
+void destroyFloatTree(FloatTree *aTree)
+{
+	if (NULL != aTree)
+	{
+		destroyNode(aTree->root);
+		free(aTree);
+	}
+}
+void insertFloatValueToTree(FloatTree *aTree, float aValue)
+{
+	if (NULL == aTree)
+	{
+		return;
+	}
+	FloatNode *theNode = createFloatNodeWithValue(aValue);
+	if (NULL == theNode)
+	{
+		return;
+	}
+
+	if (NULL == aTree->root)
+	{
+		aTree->root = theNode;
+		aTree->count ++;
+	}
+	else
+	{
+		FloatNode *theCurrent = aTree->root;
+		FloatNode *theParent = NULL;
+	while (1)
+		{
+			theParent = theCurrent;
+
+			if (aValue < theCurrent->value)
+			{
+				theCurrent = theCurrent->leftChild;
+				if (NULL == theCurrent)
+				{
+					theParent->leftChild = theNode;
+					break;
+				}
+			}
+			else
+			{
+				theCurrent = theCurrent->rightChild;
+				if (NULL == theCurrent)
+				{
+					theParent->rightChild = theNode;
+					break;
+				}
+			}
+		}
+
+		aTree->count ++;
+	}
+}
+FloatNode *findNodeWithValue(FloatTree *aTree, float aValue)
+{
+	FloatNode *theCurrentNode = NULL;
+
+	if (NULL != aTree && NULL != aTree->root)
+	{
+		FloatNode *theCurrentNode = aTree->root;
+		while (aValue != theCurrentNode->value)
+		{
+			theCurrentNode = (aValue < theCurrentNode->value)
+						? theCurrentNode->leftChild
+						: theCurrentNode->rightChild;
+
+			if (NULL == theCurrentNode)
+			{
+				break;
+			}
+		}
+	}
+
+	return theCurrentNode;
+}
+
+void deleteNodeWithValue(FloatTree *aTree, float aValue) {
+
+
+	if (NULL == aTree || NULL == aTree->root) {
+		return ;
+	}
+
+	FloatNode *current = aTree->root;
+	FloatNode *parent = aTree->root;
+
+
+	while (aValue != current->value) {
+		parent = current;
+		if (aValue < current->value) {
+			current = current->leftChild;
+		} else {
+			current = current->rightChild;
+		}
+
+		if (NULL == current) {
+			return ;
+		}
+	}
+
+	if (NULL == current->leftChild && NULL == current->rightChild) {
+		if (aTree->root == current) {
+			aTree->root = NULL;
+		} else if (parent->leftChild == current) {
+			parent->leftChild = NULL;
+		} else {
+			parent->rightChild = NULL;
+		}
+
+		aTree->count --;
+	} else if (NULL == current->rightChild) {
+		if (aTree->root == current) {
+			aTree->root = current->leftChild;
+		} else if (parent->leftChild == current) {
+			parent->leftChild = current->leftChild;
+		} else {
+			parent->rightChild = current->rightChild;
+		}
+	} else if (NULL == current->leftChild) {
+		if (aTree->root == current) {
+			aTree->root = current->rightChild;
+		} else if (parent->rightChild == current) {
+		 	parent->rightChild = current->rightChild;
+		} else {
+			parent->leftChild = current->leftChild;
+		}
+	} else {
+		FloatNode *successor = getSuccessor(aTree, current);
+		if (aTree->root == successor) {
+			aTree->root = NULL;
+		} else if (parent->leftChild == current) {
+			parent->leftChild = successor;
+		} else {
+			parent->rightChild = successor;
+		}
+		current->leftChild = NULL;
+		current->rightChild = NULL;
+	}
+
+	destroyNode(current);
+}
+
+void printTree(FloatTree *aTree) {
+	FloatNode* item = aTree->root;
+	print_float_node(item);
+}
+
+void print_float_node(FloatNode *node) {
+	if (node == NULL)
+		return;
+	print_float_node(node->leftChild);
+	printf("element: %f\n", node->value);
+	print_float_node(node->rightChild);
+}
+
+void destroyNode(FloatNode *aNode)
+{
+	if (NULL != aNode)
+	{
+		destroyNode(aNode->leftChild);
+		destroyNode(aNode->rightChild);
+
+		free(aNode);
+	}
+}
+
+FloatNode *createFloatNodeWithValue(float aValue)
+{
+	FloatNode *theNode = (FloatNode *)malloc(sizeof(FloatNode));
+
+	if (NULL != theNode)
+	{
+		theNode->leftChild = NULL;
+		theNode->rightChild = NULL;
+		theNode->value = aValue;
+	}
+
+	return theNode;
+}
+
+FloatNode *getSuccessor(FloatTree *tree, FloatNode *toDelete) {
+	FloatNode *successParent = toDelete;
+	FloatNode *successor = toDelete;
+	FloatNode *current = toDelete->rightChild;
+
+	while(NULL != current) {
+		successParent = successor;
+		successor = current;
+		current = current->leftChild;
+	}
+	if (successor != toDelete->rightChild) {
+		successParent->leftChild = successor->rightChild;
+		successor->rightChild = toDelete->rightChild;
+	}
+	successor->leftChild = toDelete->leftChild;
+	return successor;
+}
+

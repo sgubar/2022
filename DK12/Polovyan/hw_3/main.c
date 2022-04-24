58250856a7a84eb426a09282edbbcbb2ddeb21e5
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-04-24 20:06:50 +0300

    Update main.c

--- DK12/Polovyan/hw_3/main.c
+++ DK12/Polovyan/hw_3/main.c
@@ -7,7 +7,7 @@
 // эллипс 
 
 int main(void) {
-	Point *center = createPoint(3, 2);
+	Point *center = createPoint(3, 2); 
 	Point *edge_a = createPoint(1, 4);
 	Point *edge_b = createPoint(6, 5);
 	Elips *elipsOne = createElips(center, edge_a, edge_b);
@@ -28,11 +28,32 @@ int main(void) {
 
     int index0 = addElipsToArray(elipsArray, elipsOne);
     int index1 = addElipsToArray(elipsArray, elipsTwo);
-    int index2 = addElipsToArray(elipsArray, elipsOne);
+    int index2 = addElipsToArray(elipsArray, elipsThree); 
     int index3 = addElipsToArray(elipsArray, elipsThree);
+    
+    
+    printf("\n1st index: %d\n", index0);
+    printf("2nd index: %d\n", index1);
+    printf("3rd index: %d\n", index2);
+    printf("4th index: %d\n\n", index3);
 
-    printf("One: %d\n", index0);
-    printf("Two: %d\n", index1);
+    printDetailElips(getElipsByIndex(elipsArray, 0));
+    // empty so print nothing
+
+    index2 = setElipsAtIndex(elipsArray, elipsThree , 2);
+
+    printDetailElips(getElipsByIndex(elipsArray, 2));
+
+    printf("Arrays: %d\n\n", countElipsArray(elipsArray));
+   
+    printElipsArray(elipsArray);
+
+    deleteElipsArray(elipsArray);
+//===========================================================================================================================//
+	return 1;
+}
+// в этом методе мы строим фигуру по трём точкам: center - центр эллипса, edge_a - крайняя точка первой полуоси, edge_b - крайняя точка второй полуоси
+// точки должны быть построены так: если от center к edge_a и edge_b провести линии - эти линии будут перпендикулярны, только так получается эллипс
     printf("Three: %d\n", index2);
     printf("Four: %d\n", index3);
 

0167cd3f4be61040e03f83b78307a85b38cf892b
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-04-26 18:39:42 +0300

    Update dk12_tool.h

--- DK12/Polovyan/lab_2/dk12_tool.h
+++ DK12/Polovyan/lab_2/dk12_tool.h
@@ -1,1 +1,34 @@
+#ifndef dk12_tool_h
+#define dk12_tool_h
 
+//====================================================================//
+typedef struct tagIntListElement IntListElement;
+struct tagIntListElement {
+	int value;
+	IntListElement* prev;
+	IntListElement* next;
+};
+
+IntListElement* createIntListElement(int value);
+
+void deleteIntListElement(IntListElement* element);
+
+void printIntListElement(IntListElement* element);
+
+typedef struct tagIntList {
+	IntListElement* head;
+	IntListElement* tail;
+} IntList;
+
+IntList* createIntList(void);
+
+void deleteIntList(IntList* list);
+
+void printIntList(IntList* list);
+
+int addIntValueToIntList(IntList* list, int value);
+//====================================================================//
+
+void Search(IntList* list);
+
+#endif 

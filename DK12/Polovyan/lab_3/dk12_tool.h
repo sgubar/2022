7f4ce9eb52640e2b5a9d08daa8ef4734de756a86
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-05-02 20:49:21 +0300

    Update dk12_tool.h

--- DK12/Polovyan/lab_3/dk12_tool.h
+++ DK12/Polovyan/lab_3/dk12_tool.h
@@ -1,1 +1,27 @@
+#ifndef dk12_tool_h
+#define dk12_tool_h
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct __node intNode;
+struct __node //гілки дерева
+{
+	int value;
+	intNode *leftChild;
+	intNode *rightChild;
+};
+
+typedef struct __tree // елементи дерева
+{
+	intNode *root;
+	int count;
+} intTree;
+
+intTree *createIntTree();										  // створення дерева
+void deleteIntTree(intTree *aTree);							  // видалення дерева
+void printTree(intNode *aRoot);								  // вивід дерева
+void insertIntValueToTree(intTree *aTree, int aValue);  // вставка елемента у дерево
+int deleteValueFromTree(intTree *aTree, int aValue);	  // видалення елемента з дерева
+intNode *findNodeWithValue(intTree *aTree, int aValue); // пошук елемента у дереві по значенню
+
+#endif

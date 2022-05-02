c5c96a597b7c6620b4b58725c71836e828bce689
Artemstudent123 <90600280+Artemstudent123@users.noreply.github.com>
2022-05-02 09:39:11 +0300

    Create square.h

---
+++ DK11/Artem Kochik/Homework1/Hom2(2)/Hom2 (3)/square.h
@@ -0,0 +1,26 @@
+#ifndef square_h
+#define Square_h
+
+typedef struct tag_point
+{
+    double x;
+    double y;
+} Point;
+
+typedef struct tag_square
+{
+    Point *point1;
+    Point *point2;
+} Square;
+
+Point *create_point(double x, double y);
+
+Square *create_square(Point *point1, Point *point2);
+Square *create_squareXY(double point1x, double point1y, double point2x, double point2y);
+Square *create_arr_square(Point *array[]);
+
+double area_square(Square *square);
+void square_destroy(Square *square);
+void square_info(Square *square, double area);
+
+#endif

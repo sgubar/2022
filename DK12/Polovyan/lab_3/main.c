2a90661f3af45f2ab48edfd0d860d7ef837059a5
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-05-02 20:48:37 +0300

    Update main.c

--- DK12/Polovyan/lab_3/main.c
+++ DK12/Polovyan/lab_3/main.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include "dk12_tool.h"
 
+int main()
+{
+	intTree *LAB3 = createIntTree(); // створюємо дерево
+
+	insertIntValueToTree(LAB3, 17); // дадаємо єлементи у дерево
+	insertIntValueToTree(LAB3, 2);
+	insertIntValueToTree(LAB3, 8);
+	insertIntValueToTree(LAB3, 97);
+	insertIntValueToTree(LAB3, 3);
+	insertIntValueToTree(LAB3, 7);
+	insertIntValueToTree(LAB3, 4);
+	insertIntValueToTree(LAB3, 1);
+
+	printf("\n");
+	printTree(LAB3->root); // друкуємо єлементи дерева
+
+	printf("\n\nNode : %d\n\n", findNodeWithValue(LAB3, 2)->value); // знаходимо елемент у початковому дереві
+
+	deleteValueFromTree(LAB3, 97); // видаляємо єлементи дерева
+	deleteValueFromTree(LAB3, 7);
+
+	printTree(LAB3->root);																	  // друкуємо єлементи дерева, які залишились
+	printf("\n\nNode %d still exist\n\n", findNodeWithValue(LAB3, 2)->value); // знаходимо елемент в новому дереві
+	deleteIntTree(LAB3); // видаляємо дерево
+}

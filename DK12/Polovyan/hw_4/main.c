be0551f2af190ff6aeb6557fd7ca188d27a16ba0
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-05-05 15:50:08 +0300

    Update main.c

--- DK12/Polovyan/hw_4/main.c
+++ DK12/Polovyan/hw_4/main.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include "dk12_tool.h"
 
+int main(int argc, const char *argv[])
+{
+
+	FloatList *list = createFloatList(); // create float list
+
+	printf("\n");
+	addValueToFloatList(list, 2.53); // add elements
+	addValueToFloatList(list, 4.3);
+	printFloatList(list); // print new list
+
+	insertElementToFloatListAtIndex(list, 0, 0.0); // add elements in chosen place
+	insertElementToFloatListAtIndex(list, 1, 54.2);
+	insertElementToFloatListAtIndex(list, 2, 97.2);
+	insertElementToFloatListAtIndex(list, 3, -15.43);
+	insertElementToFloatListAtIndex(list, 4, 57.99);
+	printFloatList(list);
+
+	removeFirstElementFromFloatList(list); // remove first element
+	printFloatList(list);
+	removeElementFromFloatListAtIndex(list, 3); // remove element from chosen place
+	printFloatList(list);
+	removeLastElementFromFloatList(list); // remove last element
+	printFloatList(list);
+
+	deleteFloatList(list); // delete float list
+	return 0;
+}

342129f99bc9b7e3238962042e7ae72139d351b0
Artemstudent123 <90600280+Artemstudent123@users.noreply.github.com>
2022-05-06 10:11:13 +0300

    Create main.c

---
+++ DK11/Artem Kochik/Homework1/Hom4/main.c
@@ -0,0 +1,19 @@
+#include "float.h"
+
+int main()
+{
+    FloatSingleList *list = createFloatSingleList();
+    printFloatSingleList(list);
+
+    addFloatValueToSingleList(list, 5.867448);
+    addFloatValueToSingleList(list, 2.237897);
+    printFloatSingleList(list);
+    removeFirstElementFromFloatSingleList(list);
+    printFloatSingleList(list);
+    removeLastElementFromFloatSingleList(list);
+    printFloatSingleList(list);
+
+    printf("Hello world!\n");
+    deleteFloatSingleList(list);
+    return 0;
+}

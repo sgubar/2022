238268c8fe75354050e273718f0ac2142bbd97cb
Lay <91009566+LayTreE@users.noreply.github.com>
2022-05-08 23:14:06 +0300

    Create inttree.h

---
+++ DK11/Danylovich/Hometask05/inttree.h
@@ -0,0 +1,36 @@
+#ifndef inttree_h
+#define inttree_h
+
+#include <stdio.h>
+
+typedef struct tag_Node
+{
+	int value;
+	struct tag_Node *leftChild;
+	struct tag_Node *rightChild;
+}IntNode;
+
+void destroyNode(IntNode *aNode);
+IntNode *createIntNodeWithValue(int aValue);
+
+typedef struct tag_Tree
+{
+	IntNode *root;
+	int count;
+}IntTree;
+
+IntNode *getSuccessor(IntTree *tree, IntNode *toDelete);
+void print_int_node(IntNode *node);
+IntTree *createIntTree();
+void destroyIntTree(IntTree *aTree);
+
+void insertIntValueToTree(IntTree *aTree, int aValue);
+IntNode *findNodeWithValue(IntTree *aTree, int aValue);
+void deleteNodeWithValue(IntTree *aTree, int aValue);
+
+void mergeTrees(IntTree *aTreeDst, IntTree *aTreeSrc);
+
+void printTree(IntTree *aTree);
+int countNodesWithTree(IntTree *aTree);
+
+#endif

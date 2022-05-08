334b9459fa02424057f03b06a676dfc9d6b8e992
Denis Polovian <91053416+Denisqb@users.noreply.github.com>
2022-05-08 19:20:15 +0300

    Update main.c

--- DK12/Polovyan/hw_5/main.c
+++ DK12/Polovyan/hw_5/main.c
@@ -3,26 +3,26 @@
 
 int main()
 {
-	floatTree *LAB3 = createFloatTree(); // створюємо дерево
+	floatTree *HW5 = createFloatTree(); // створюємо дерево
 
-	insertFloatValueToTree(LAB3, 17.2); // дадаємо єлементи у дерево
-	insertFloatValueToTree(LAB3, 24.1);
-	insertFloatValueToTree(LAB3, 8);
-	insertFloatValueToTree(LAB3, 97.0);
-	insertFloatValueToTree(LAB3, 3.3);
-	insertFloatValueToTree(LAB3, 7.97);
-	insertFloatValueToTree(LAB3, 4.32);
-	insertFloatValueToTree(LAB3, 1.00);
+	insertFloatValueToTree(HW5, 17.2); // дадаємо єлементи у дерево
+	insertFloatValueToTree(HW5, 24.1);
+	insertFloatValueToTree(HW5, 8);
+	insertFloatValueToTree(HW5, 97.0);
+	insertFloatValueToTree(HW5, 3.3);
+	insertFloatValueToTree(HW5, 7.97);
+	insertFloatValueToTree(HW5, 4.32);
+	insertFloatValueToTree(HW5, 1.00);
 
 	printf("\n");
-	printTree(LAB3->root); // друкуємо єлементи дерева
+	printTree(HW5->root); // друкуємо єлементи дерева
 
-	printf("\n\nNode : %.2f\n\n", findNodeWithValue(LAB3, 4.32)->value); // знаходимо елемент у початковому дереві
+	printf("\n\nNode : %.2f\n\n", findNodeWithValue(HW5, 4.32)->value); // знаходимо елемент у початковому дереві
 
-	deleteValueFromTree(LAB3, 24.1); // видаляємо єлементи дерева
-	deleteValueFromTree(LAB3, 7.97);
+	deleteValueFromTree(HW5, 24.1); // видаляємо єлементи дерева
+	deleteValueFromTree(HW5, 7.97);
 
-	printTree(LAB3->root);																	  // друкуємо єлементи дерева, які залишились
-	printf("\n\nNode %.2f still exist\n\n", findNodeWithValue(LAB3, 4.32)->value); // знаходимо елемент в новому дереві
-	deleteFloatTree(LAB3); // видаляємо дерево
+	printTree(HW5->root);																	  // друкуємо єлементи дерева, які залишились
+	printf("\n\nNode %.2f still exist\n\n", findNodeWithValue(HW5, 4.32)->value); // знаходимо елемент в новому дереві
+	deleteFloatTree(HW5); // видаляємо дерево
 }

f903bd73d095e5f6ae5ef5eb0f56b626fca0905e
Osmirko22 <98734472+Osmirko22@users.noreply.github.com>
2022-05-15 20:40:41 +0300

    Create dk11_tool.h

---
+++ DK11/Osmirko/homework5/dk11_tool.h
@@ -0,0 +1,37 @@
+#ifndef _DK11_TOOL_H
+#define _DK11_TOOL_H
+
+typedef struct tag_data
+{
+	char *data;
+}Data, *DataPtr;
+
+typedef struct tag_node
+{
+	DataPtr toData;
+	struct tag_node *leftChild;
+	struct tag_node *rightChild;
+}Node, *NodePtr;
+
+typedef struct tag_tree
+{
+	NodePtr root;
+}Tree, *TreePtr;
+
+TreePtr createTree();
+void deleteTree(TreePtr tree);
+
+int countTree(TreePtr tree);
+void displayTree(TreePtr tree);
+
+NodePtr findNode(TreePtr tree, char *data);
+void insertNode(TreePtr tree, char *data);
+int deleteNode(TreePtr tree, char *data);
+
+void destroyNode(NodePtr node);
+void destroyTree(TreePtr tree);
+
+void printTree(TreePtr tree);
+void printCharNode(NodePtr node);
+
+#endif

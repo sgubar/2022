47e1e1ae9c335a6d24b52b7fb740e32c62d42737
Dmitry <95189271+DmitryDegtiarov2812@users.noreply.github.com>
2022-05-18 20:40:57 +0300

    add Dehtiarov's folder

---
+++ DK12/Dehtiarov/Лаб.1/functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+/*int enterA();
+int enterB();
+int enterC();*/
+int enterABC();
+int factorial(int x);
+float sum(int C, int A);
+
+
+#endif // FUNCTIONS_H_INCLUDED

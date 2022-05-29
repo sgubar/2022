ee5aaf6a1a2d0f694240d892920a50d963bfebcd
Sochnyi <91053509+Sochnyi@users.noreply.github.com>
2022-05-29 16:37:56 +0300

    Update dk_tool.h

--- DK11/Сергій Сопчинський/Практичні роботи.md/Лабораторна №2/dk_tool.h
+++ DK11/Сергій Сопчинський/Практичні роботи.md/Лабораторна №2/dk_tool.h
@@ -1,1 +1,22 @@
+#ifndef dk_tool_h
+#define dk_tool_h
 
+typedef struct node
+{
+    struct node *prev;
+    char *symbol;
+    struct node *next;
+}
+void printPrompt(void);
+
+Node *createNode(Node *prev, char *s);
+
+int openingdyzhka(char *symbol);
+int closingdyzhka(char *symbol);
+
+int correctclosingdyzhka(Node *n, char *s);
+
+void notBalanced(void);
+void balanced(void);
+
+#endif

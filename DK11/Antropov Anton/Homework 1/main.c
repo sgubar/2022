d28c26f7ac86ea9d1978b92c0e89601bd49ed95e
dk11antropov <98733538+dk11antropov@users.noreply.github.com>
2022-06-12 04:02:31 +0300

    Update main.c

--- DK11/Antropov Anton/Homework 1/main.c
+++ DK11/Antropov Anton/Homework 1/main.c
@@ -1,1 +1,17 @@
+#include "dk_tool.h"
+int main ()
+{
+    int n, m, i, j, chyslo;
+    printf("Введіть розмір масиву : ");
+    scanf("%d %d", &n, &m);
+
+    printf("На яке число будемо множити матрицю? ");
+    scanf("%d", &chyslo);
+
+    int **array = create_matrix(n,m);
+    fillarray((int **)array,n,m,chyslo);
+    printarray((int **)array,n,m);
+
+    delete_matrix((int **)array,n);
+}
 

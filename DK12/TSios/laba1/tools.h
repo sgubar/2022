9604c087221a4de7080c8548c86e473c46a7b116
tsiosn <99125681+tsiosn@users.noreply.github.com>
2022-06-18 22:32:15 +0300

    Create tools.h

---
+++ DK12/TSios/laba1/tools.h
@@ -0,0 +1,13 @@
+#ifndef tools_h
+#define tools_h
+
+int getA(void);
+int getB(void);
+int getC(void);
+
+int getSum(int a);
+double calcResult(int a,int b,int c, int sum);
+
+void printResult(double res);
+
+#endif

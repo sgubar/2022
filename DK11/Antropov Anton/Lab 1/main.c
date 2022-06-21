57cdca79b6d544cecc09e9da5f33dc7177471b42
dk11antropov <98733538+dk11antropov@users.noreply.github.com>
2022-06-21 13:26:01 +0300

    Create main.c

---
+++ DK11/Antropov Anton/Lab 1/main.c
@@ -0,0 +1,9 @@
+#include "dk_tool.h" 
+int main (int argc, const char * argv[])
+{
+    float a = result();
+    if(a == 0){
+        return 0;
+    } else
+    printf("Розв'язок задачі : %f\n",a);
+}

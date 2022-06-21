6dddc82a222c095dfafc5393956f5a7d3a9123ce
dk11antropov <98733538+dk11antropov@users.noreply.github.com>
2022-06-21 13:57:35 +0300

    Update list.h

--- DK11/Antropov Anton/Lab 2/list.h
+++ DK11/Antropov Anton/Lab 2/list.h
@@ -1,1 +1,40 @@
+#ifndef list_h
+#define list_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+	struct Node *next;
+}Node;
+
+
+typedef struct IntSingleList {
+	Node *head;
+}IntSingleList;
+
+Node *createSingleListIntElement(int value);
+void deleteSingleListIntElement(Node *element);
+void printSingleListIntElement(Node *element);
+
+IntSingleList *createIntSingleList(void);
+void deleteIntSingleList(IntSingleList *list);
+
+void printIntSingleList(IntSingleList *list);
+
+long countOfIntSingleList(IntSingleList *list);
+
+int addIntValueToIntSingleList(IntSingleList *list, int value);
+void removeLastElementFromIntSingleList(IntSingleList *list);
+void removeFirstElementFromIntSingleList(IntSingleList *list);
+int removeIntElementsLessThanAverageValue(IntSingleList *list, int MNumber);
+int averageValueOfIntSingleList(IntSingleList *list);
+IntSingleList *getIntElementToIntSingleList();
+
+int insertIntElementToIntSingleListAtIndex(IntSingleList *list, int index, int value);
+
+int removeIntElementFromIntSingleListAtIndex(IntSingleList *list, int index);
+
+#endif // LIST_H_INCLUDED
 

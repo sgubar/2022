ea8293890a2615675240963a088301dd54258a89
dk11antropov <98733538+dk11antropov@users.noreply.github.com>
2022-06-21 15:04:54 +0300

    Update inttree.h

--- DK11/Antropov Anton/Lab 3/inttree.h
+++ DK11/Antropov Anton/Lab 3/inttree.h
@@ -1,1 +1,38 @@
+#ifndef inttree_h
+#define inttree_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+	int value;
+	struct Node *leftChild;
+	struct Node *rightChild;
+}IntNode;
+
+void destroyNode(IntNode *aNode);
+IntNode *createIntNodeWithValue(int aValue);
+
+typedef struct intTree
+{
+	IntNode *root;
+	int count;
+}IntTree;
+
+IntNode *getSuccessor(IntTree *tree, IntNode *toDelete);
+void print_int_node(IntNode *node);
+IntTree *createIntTree();
+void destroyIntTree(IntTree *aTree);
+
+void insertIntValueToTree(IntTree *aTree, int aValue);
+IntNode *findNodeWithValue(IntTree *aTree, int aValue);
+void deleteNodeWithValue(IntTree *aTree, int aValue);
+
+void mergeTrees(IntTree *aTreeDst, IntTree *aTreeSrc);
+
+void printTree(IntTree *aTree);
+int countNodesWithTree(IntTree *aTree);
+
+#endif
 

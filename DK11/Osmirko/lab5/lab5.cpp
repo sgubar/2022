0fc867c4917251f8b1a6714031bc08e67914d611
Osmirko22 <98734472+Osmirko22@users.noreply.github.com>
2022-06-23 13:34:08 +0300

    Create lab5.cpp

---
+++ DK11/Osmirko/lab5/lab5.cpp
@@ -0,0 +1,59 @@
+// ВАРІАНТ 1
+
+#include <iostream>
+#include <string.h>
+
+class Point {
+private:
+    float x, y; 
+
+public:
+    void set_CoordX(float new__x) { 
+        x = new__x;
+    }
+    void set_CoordY(float new__y) {
+        y = new__y;
+    }
+
+    int get_X() { // селектор
+        return x;
+    }
+
+    int get_Y() {
+        return y;
+    }
+
+    Point(float new__x, float new__y) { 
+        x = new__x;
+        y = new__y;
+        display();
+    }
+
+    Point() { 
+        x = 0;
+        y = 0;
+    }
+
+    void display() { 
+        std::cout << "\nPoint X: " << x << ", Point Y: " << y << "\n";
+    }
+
+    ~Point() { 
+        std::cout << "Destroyed\n";
+    }
+};
+
+int main(void) {
+    Point onePoint;
+    onePoint.set_CoordX(-3.4567);
+    onePoint.set_CoordY(9.67);
+    onePoint.display();
+    std::cout << "Coords: [" << onePoint.get_X() << ", " << onePoint.get_Y() << "]\n" << std::endl;
+
+    Point twoPoint(0, -10);
+
+    Point copy__twoPoint = Point(twoPoint);
+    copy__twoPoint.display();
+
+    return 1;
+}

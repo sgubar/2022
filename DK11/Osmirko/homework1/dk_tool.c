a82a043b03605717f75346668c47535efadde16c
Osmirko22 <98734472+Osmirko22@users.noreply.github.com>
2022-06-24 16:26:11 +0300

    Update dk_tool.c

--- DK11/Osmirko/homework1/dk_tool.c
+++ DK11/Osmirko/homework1/dk_tool.c
@@ -1,13 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "dk_tool.h"
 
-int powered(int a)
+int processingBinaryNumber(void)
 {
-    int i=0, j=1, m=1, d=2;
-    while(i<a)
+    char numberArray[8];
+    int count = 0;
+    char number;
+    printf("Введіть двікове число\n");
+    while (scanf("%c", &number) == 1)
     {
-        m=j*d;
-        j=m;
-        i++;
+        if (number != '0' && number != '1')
+        {
+            if (number == '\n')
+                break;
+            printf("Тільки число двійкової системи!");
+            exit(0);
+        }
+        numberArray[count] = number;
+        count++;
+        if (count > 8)
+        {
+            printf("Максимум 8 цифр!");
+            exit(0);
+        }
     }
-    return j;
+
+    int result = 0;
+    int i = 0;
+    while (count != 0)
+    {
+        result = result * 2 + (numberArray[i] - '0');
+        i++;
+        count--;
+    }
+    return result;
+}
+
+void printResult(int result)
+{
+    printf("Результат: %i\n", result);
 }

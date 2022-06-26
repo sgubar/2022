23b7ae123fdc0da99af24a651f566e4974193002
Danil Dubok <91052633+DanilDubok@users.noreply.github.com>
2022-06-26 11:30:19 +0300

    Add files via upload

---
+++ DK11/Dubok Danil/Homework/HW_2/trap.h
@@ -0,0 +1,24 @@
+#ifndef trap_h
+#define trap_h
+#include "point.h"
+#include "math.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct inf_t{
+     IP *A;
+     IP *B;
+     IP *C;
+     IP *D;
+} IT;
+
+IT *createTrap(IP *p1, IP *p2, IP *p3, IP *p4);
+void delitTrap(IT *t);
+void printTrap(IT *t);
+int inputC(char a, char b);
+float S(IT *t);
+void help();
+
+#endif
+

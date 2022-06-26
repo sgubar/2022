d5db476a437aa54e2b73b00d65d9d2112c74c197
Sochnyi <91053509+Sochnyi@users.noreply.github.com>
2022-06-27 01:38:35 +0300

    Update main.cpp

--- DK11/Сергій Сопчинський/Практичні роботи.md/Лабораторна №4/main.cpp
+++ DK11/Сергій Сопчинський/Практичні роботи.md/Лабораторна №4/main.cpp
@@ -1,10 +1,16 @@
+/*
+14 варіант
+ рух графіку стрілками
+ вибір масштабу Pg Up та Pg Dn
+*/
+
 #include <GL/glut.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-double zoom = 1.5;
+double zoom = 1.5; //вибір масштабу
 double l, u;
-void ZoomKeys(int key, int x, int y)
+void ZoomKeys(int key, int x, int y) //рух положення графіка та зміна масштабу
 {
 	switch (key)
 	{
@@ -32,8 +38,8 @@ void Graf()
 {
     int a = 1;
 	glBegin(GL_LINE_STRIP);
-	glColor3f(0.0, 0.0, 1.0);
-	for (double t = -2.5*M_PI; t <= 2.5*M_PI; t += 0.01)
+	glColor3f(0.0, 0.0, 1.0); // колір графіка
+	for (double t = -2.5*M_PI; t <= 2.5*M_PI; t += 0.01) //формули за варіантом на заданих межах
 	{
 		float x = a *  cos(t / 5) * cos(t);
 		float y = a *  cos(t / 5) * sin(t);
@@ -41,7 +47,7 @@ void Graf()
 	}
 	glEnd();
 }
-void LineX()
+void LineX() //колір вісі Х
 {
 	glBegin(GL_LINES);
 	glColor3f(0.0, 1.0, 0.0);
@@ -49,7 +55,7 @@ void LineX()
 	glVertex2f(1, 0);
 	glEnd();
 }
-void LineXY(double x)
+void LineXY(double x) //колір поділок вісі Х
 {
 	glBegin(GL_LINES);
 	glColor3f(0.0, 1.0, 0.0);
@@ -57,7 +63,8 @@ void LineXY(double x)
 	glVertex2f(x, -0.2);
 	glEnd();
 }
-void LineY() {               // цвет оси Y
+void LineY() //колір осі Y
+{ 
 	glBegin(GL_LINES);
 	glColor3f(0.0, 1.0, 0.0);
 	glVertex2f(0, -1);
@@ -65,7 +72,7 @@ void LineY() {               // цвет оси Y
 	glEnd();
 }
 
-void LineYX(double y)
+void LineYX(double y) //колір поділок на вісі Y
 {
 	glBegin(GL_LINES);
 	glColor3f(0.0, 1.0, 0.0);

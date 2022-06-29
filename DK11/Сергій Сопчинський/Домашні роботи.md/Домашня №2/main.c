fd5a5dffc8a66f8f41f01a3e3288a030301f145a
Sochnyi <91053509+Sochnyi@users.noreply.github.com>
2022-06-29 22:09:45 +0300

    Update main.c

--- DK11/Сергій Сопчинський/Домашні роботи.md/Домашня №2/main.c
+++ DK11/Сергій Сопчинський/Домашні роботи.md/Домашня №2/main.c
@@ -1,1 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "dk_tool.h"
 
+int main()
+{
+	Point* O = createPoint(40, 40);
+	Point* M = createPoint(33, 12);
+	double radius = 13;
+	Circle* circleA = createCircle(M, radius);
+	Circle* circleB = createCirclePoints(O, M);
+	Circle* circleC = createCircleCoord(23, -4, -11, 1);
+	infoCircle(circleA);
+	infoCircle(circleB);
+	infoCircle(circleC);
+	destroyCircle(circleA);
+	destroyCircle(circleB);
+	destroyCircle(circleC);
+	return 0;
+}

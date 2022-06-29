c9563c7d534afe32ea28d088cb44c9fbfd4ebdbb
Ogark <91046770+Ogark@users.noreply.github.com>
2022-06-29 23:35:34 +0300

    Create matrix.h

---
+++ DK11/Garkavenko/HW1(NEW)/matrix.h
@@ -0,0 +1,10 @@
+#ifndef matrix_h
+#define matrix_h
+
+int enter_size_matrixrow();
+int enter_size_matrixcol();
+void enter_size_matrix1(int* m1, int x, int y);
+void enter_size_matrix2(int* m2, int x, int y);
+void matrixsum(int* m1, int* m2, int x, int y);
+
+#endif

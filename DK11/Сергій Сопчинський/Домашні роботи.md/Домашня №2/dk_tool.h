efd10988608947181982a02aee0b6b7ede193fdc
Sochnyi <91053509+Sochnyi@users.noreply.github.com>
2022-06-29 22:10:27 +0300

    Update dk_tool.h

--- DK11/Сергій Сопчинський/Домашні роботи.md/Домашня №2/dk_tool.h
+++ DK11/Сергій Сопчинський/Домашні роботи.md/Домашня №2/dk_tool.h
@@ -1,1 +1,23 @@
+#ifndef dk_tool_h
+#define dk_tool_h
 
+typedef struct tag_point
+{
+	double x;
+	double y;
+} Point;
+
+typedef struct tag_circle
+{
+	Point* center;
+	double radius;
+} Circle;
+Point* createPoint(double x, double y);
+Circle* createCircle(Point* center, double radius);
+Circle* createCirclePoints(Point* center, Point* radius);
+Circle* createCircleCoord(double centerX, double centerY, double x, double y);
+double areaCircle(Circle* circle);
+void destroyCircle(Circle* circle);
+void infoCircle(Circle* circle);
+
+#endif

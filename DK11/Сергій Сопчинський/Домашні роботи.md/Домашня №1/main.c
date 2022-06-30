ec68ce8bb21f8ecb336d84d7a3be7e1ace8aba70
Sochnyi <91053509+Sochnyi@users.noreply.github.com>
2022-06-30 19:36:40 +0300

    Update main.c

--- DK11/Сергій Сопчинський/Домашні роботи.md/Домашня №1/main.c
+++ DK11/Сергій Сопчинський/Домашні роботи.md/Домашня №1/main.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
 #include "dk_tool.h"
-int main()
+
+void main()
 {
-	printf_size1(); рядок
-  	matrix1_elements(); цифра р
- 	printf_size2 (); стовпчик
-  	matrix2_elements(); цифра с
-	multiple ();
-	result ();
-	return 0;
+      int x, y, z;
+      printf("Введіть розмір матриці\n");
+      printf("[x]: ");
+      scanf("%d", &x);
+      printf("[y]: ");
+      scanf("%d", &y);
+      printf("Цифра множення");
+      scanf("%d", &z);
+      int mat[x][y];
+      inputMat(x, y, mat);
+      mulMat(z, x, y, mat);
+      printMulMat(x, y, mat);
 }
